a63b6bd2c174cad063876c333dc9a1cf48aafb03
M M Salman <200926@ku.ac.bd>
2023-05-16 15:55:03 +0600

    11

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const arr1 = [4, 5, 6, 7, 9]
-const arr2 = [1, 2, 3, 3, 5]
+const arr1 = [4, 5, 6, 7, 9];
+const arr2 = [1, 2, 3, 3, 5];
 
 let termCredits = 0;
 
 for (let i = 0; i < 4; i++) {
   if (arr1[i] > 4) {
-    termCredits = termCredits + arr2[i]
+    termCredits = termCredits + arr2[i];
   }
 }
 
-console.log(termCredits)
-console.log(`main brunch from pc again and again and again`);
+console.log(termCredits);
+console.log(`main brunch from pc and mmsalman26`);
