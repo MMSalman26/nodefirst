6b9681a822ff4a067ce98a12daaaa8c8f38a4a7c
M-MM11 <23134404-M-MM11@users.noreply.replit.com>
2023-05-16 08:44:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const arr1 = [4, 5, 6, 7]
+const arr2 = [1, 2, 3, 3, 5]
+
+let termCredits = 0;
+
+for (let i = 0; i < 4; i++) {
+  if (arr1[i] > 4) {
+    termCredits = termCredits + arr2[i]
+  }
+}
+
+console.log(termCredits)
+console.log(termCredits)
+console.log(termCredits)
+console.log(termCredits)
